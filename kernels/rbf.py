4bddf290e9e587db2af226f6138b886f5a977550
hugcis <hmj.cisneros@gmail.com>
2019-03-01 09:10:45 +0100

    Make RBF gentler on memory

--- kernels/rbf.py
+++ kernels/rbf.py
@@ -11,6 +11,9 @@ class RBFKernel:
                 (2 * self.sigma**2)))
     
     def predict_function(self, alphas, data, X):
-        dists = np.sum((data[:, :, None] - X.T[None, :, :])**2, axis=1)
+        dists = np.zeros((data.shape[0], X.shape[0]))
+        for i in range(X.shape[0]):
+            dists[:, i] = np.sum((data[:, :] - X.T[:, i])**2, axis=1)
+
         kernel_mat = np.exp(- dists / (2 * self.sigma**2))
-        return np.sign( np.sum(alphas * kernel_mat, axis=0) )
+        return np.sign( np.sum(alphas[:, None] * kernel_mat, axis=0) )
